3d7155190e3db3e09940d7ebe6c26e89e18e69bb
sandeep <sandeep.pr3@gmail.com>
2026-01-21 09:02:50 +0530

    proj4

--- src/assets/data/mywork_data.js
+++ src/assets/data/mywork_data.js
@@ -1,6 +1,7 @@
 import project1_img from "../../assets/expense.png";
 import project2_img from "../../assets/ecommerce.png";
 import project3_img from "../../assets/cooter.jpg";
+import project4_img from "../../assets/invoice.png";
 
 const mywork_data = [
   {
@@ -17,14 +18,21 @@ const mywork_data = [
     img: project2_img,
     site: "https://e-commerce-hva7.vercel.app/",
     description:
-      "A comprehensive shopping platform featuring product listings, cart functionality, and payment integration. Developed using the MERN stack with Redux Toolkit for efficient state management.",
+    "A comprehensive shopping platform featuring product listings, cart functionality, and payment integration. Developed using the MERN stack with Redux Toolkit for efficient state management.",
   },
   {
     no: 3,
     name: "Event Management APP",
     img: project3_img,
     description:
-      "A mobile application for scheduling and managing events. Built using React Native and SQLite, allowing user to register events, upload images, and manage important dates via a calendar view.",
+    "A mobile application for scheduling and managing events. Built using React Native and SQLite, allowing user to register events, upload images, and manage important dates via a calendar view.",
+  },
+  {
+    no: 4,
+    name: "Invoice Generator",
+    site: "https://invoice-management-system-7qwo.vercel.app/",
+    img: project4_img,
+    description: "Developed a full-stack sales invoice system with customer and item management, invoice CRUD operations, and dynamic calculations. Built RESTful APIs, integrated PDF invoice generation, and used MongoDB for scalable data storage and retrieval.sample password:sarath@gmail.com,123"
   },
 ];
 
